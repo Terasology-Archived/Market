ac7290e28918c4d1965612ae14bf31975497df88
Darshan <darshantank3@gmail.com>
2019-07-21 05:48:09 +0530

    Add functions to search for purchasable prefabs and blocks

--- src/main/java/org/terasology/logic/market/MarketClientSystem.java
+++ src/main/java/org/terasology/logic/market/MarketClientSystem.java
@@ -15,21 +15,48 @@
  */
 package org.terasology.logic.market;
 
+import org.terasology.assets.management.AssetManager;
+import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.prefab.Prefab;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.input.ButtonState;
 import org.terasology.input.binds.market.MarketButton;
+import org.terasology.logic.inventory.ItemComponent;
 import org.terasology.network.ClientComponent;
 import org.terasology.registry.In;
+import org.terasology.registry.Share;
 import org.terasology.rendering.nui.NUIManager;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockExplorer;
+import org.terasology.world.block.BlockManager;
+import org.terasology.world.block.BlockUri;
+import org.terasology.world.block.family.BlockFamily;
+import org.terasology.world.block.items.BlockItemFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RegisterSystem(RegisterMode.CLIENT)
+@Share(MarketClientSystem.class)
 public class MarketClientSystem extends BaseComponentSystem {
+    private Set<Prefab> purchasablePrefabs = new HashSet<>();
+    private Set<Block> purchasableBlocks = new HashSet<>();
+    private BlockItemFactory blockItemFactory;
+
     @In
     private NUIManager nuiManager;
+    @In
+    private EntityManager entityManager;
+    @In
+    private AssetManager assetManager;
+    @In
+    private BlockManager blockManager;
+
 
     @ReceiveEvent(components = ClientComponent.class)
     public void onToggleMarket(MarketButton event, EntityRef entity) {
@@ -37,5 +64,37 @@ public class MarketClientSystem extends BaseComponentSystem {
             nuiManager.toggleScreen("Market:marketScreen");
             event.consume();
         }
+    }
+
+    @Override
+    public void postBegin() {
+        blockItemFactory = new BlockItemFactory(entityManager);
+        BlockExplorer blockExplorer = new BlockExplorer(assetManager);
+
+        purchasablePrefabs = assetManager.getLoadedAssets(Prefab.class)
+                .stream()
+                .filter(prefab -> prefab.hasComponent(ItemComponent.class)
+                        && prefab.hasComponent(PurchasableComponent.class))
+                .collect(Collectors.toSet());
+
+        Set<BlockUri> blocks = new HashSet<>();
+        blocks.addAll(blockManager.listRegisteredBlockUris());
+        blocks.addAll(blockExplorer.getAvailableBlockFamilies());
+        blocks.addAll(blockExplorer.getFreeformBlockFamilies());
+
+        purchasableBlocks = blocks.stream()
+                .map(blockManager::getBlockFamily)
+                .map(BlockFamily::getArchetypeBlock)
+                .filter(block -> block.getPrefab().isPresent())
+                .filter(block -> block.getPrefab().get().hasComponent(PurchasableComponent.class))
+                .collect(Collectors.toSet());
+    }
+
+    public Set<Prefab> getPurchasablePrefabs() {
+        return purchasablePrefabs;
+    }
+
+    public Set<Block> getPurchasableBlocks() {
+        return purchasableBlocks;
     }
 }
